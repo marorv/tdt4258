25e3a30733b7c32b7800a13db8b1140b5e6ed012
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2016-09-19 09:17:51 +0200

    added more commenting

--- ex1.s
+++ ex1.s
@@ -91,87 +91,75 @@ gpio_base:
 scr_addr:
 	.long SCR
 
-loop:
-
-//get button values
-ldr r3, [r1, #GPIO_DIN]
-//left shift r3 8 bits to match the LED bitstring
-lsl r3, r3, #8
-
-//set relevant LED lights to on
-mov r5, #0x0100
-str r3, [r2, #GPIO_DOUT]
-
-bl loop
 
 	.globl  _reset
 	.type   _reset, %function
 	.thumb_func
 _reset:
 
-	ldr r1, cmu_base_addr
+	//INIT THE GPIO CLOCK
 
-	ldr r2 ,   [ r1 ,  #CMU_HFPERCLKEN0]
-	mov r3,  #1
-	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
-	orr r2,r2,r3
-	str r2, [r1,#CMU_HFPERCLKEN0]
+	ldr r1, cmu_base_addr //load cmu base to r1
+
+	ldr r2 ,   [ r1 ,  #CMU_HFPERCLKEN0] //load r2 with the current value of CMU_HFPERCLKEN0
+	mov r3,  #1 //set bit to r3
+	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO //do logical shift left on r2, put it in r3
+	orr r2,r2,r3 //do orr operation to preserve all other bits currently on CMU_HFPERCLKEN0
+	str r2, [r1,#CMU_HFPERCLKEN0] //write the new value to CMU_HFPERCLKEN0
 
 
 	//INIT LED LIGHTS
 
-	//load port a as base
-	ldr r1, gpio_pa_addr
+	ldr r1, gpio_pa_addr //load port a as base (base of the LEDs)
 
 	//set gpio drive strength to high
-	mov r2, #0x02
-	str r2, [r1, #GPIO_CTRL]
+	mov r2, #0x02 //set value for high to r2
+	str r2, [r1, #GPIO_CTRL] //store the r2 value to GPIO_CTRL
 
 	//set pins 8-15 to type output (LED)
-	mov r2, #0x55555555
-	str r2, [r1, #GPIO_MODEH]
+	mov r2, #0x55555555 //set r2 to value of type output
+	str r2, [r1, #GPIO_MODEH] //write r2 value to LED GPIO_MODEH register
 
 
 	//INIT BUTTONS
-	//load port c as base
+	//load port c as base (base of the buttons)
 	ldr r1, gpio_pc_addr
 
 	//set button pins as type input
-	mov r2, #0x33333333
-	str r2, [r1, #GPIO_MODEL]
+	mov r2, #0x33333333 //write the value that indicates input to r2
+	str r2, [r1, #GPIO_MODEL] //store the r2 value to the button GPIO_MODEL
 
 	//set r2 to 0xff and store the value to GPIO_PC_DOUT
-	mov r2, #0xff
-	str r2, [r1, #GPIO_DOUT]
+	mov r2, #0xff //set value of r2 to ff
+	str r2, [r1, #GPIO_DOUT] //store the value of r2 to button GPIO_DOUT
 
 	//INIT interrupt
 	ldr r1, gpio_base
 
 	mov r2, #0x22222222
-	str r2, [r1, #GPIO_EXTIPSELL]
+	str r2, [r1, #GPIO_EXTIPSELL] 
 	
+	//enable gpio interrupt to happen on both button fall and rise
 	mov r2, #0xff
-	str r2, [r1, #GPIO_EXTIFALL]
+	str r2, [r1, #GPIO_EXTIFALL] //store ff to GPIO_EXTIFALL to enable interrupt on button fall
+	str r2, [r1, #GPIO_EXTIRISE] //store ff to GPIO_EXTIFALL to enable interrupt on button rise
+	str r2, [r1, #GPIO_IEN] //enable gpio interrupts
 
-	mov r2, #0xff
-	str r2, [r1, #GPIO_EXTIRISE]
+	movw r2, #0x802 //set lower bits of r2 to 0x802
+	ldr r3, iser0 //load r3 with the address of NVIC ISER0
+	str r2, [r3] //store the 0x802 value to the ISER0 address.
 
-	mov r2, #0xff
-	str r2, [r1, #GPIO_IEN]
+	wfi //wait for interrupt, enabling sleep mode
 
-	movw r2, #0x802
-	ldr r3, iser0
-	str r2, [r3]
-
-	wfi
-
-	b deepsleep
+	b deepsleep //branch to deepsleep label
 
 	.thumb_func
 deepsleep:
-	ldr r1, scr_addr
-	mov r2, #6
-	str r2, [r1]
+	ldr r1, scr_addr //load the state address into r1
+	mov r2, #6 //set r2 register to value for deep sleep state
+	str r2, [r1] //store the deep sleep value to the SCR address to initiate deep sleep modus
+
+	bx lr //jump back to wfi
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -195,8 +183,8 @@ gpio_handler:
 	lsl r3, r3, #8
 
 	//set relevant LED lights to on
-	mov r5, #0x0100
-	str r3, [r2, #GPIO_DOUT]
+	mov r5, #0xff00
+	str r5, [r2, #GPIO_DOUT]
 
 	bx lr
 
