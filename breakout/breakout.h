ee445c41dc554bc57f650c279cbbb9e3b685005d
Maren Orvedal <marenor@stud.ntnu.no>
2016-11-14 15:17:13 +0100

    Adding the game in C...

--- breakout/breakout.h
+++ breakout/breakout.h
@@ -1,23 +1,45 @@
 #ifndef FILENAME_H
 
-#include <iostream>
-#include <cmath>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
-struct Ball;
-struct Pad;
-struct Frame;
-struct Brick;
+typedef struct{
+	float x;
+	float y;
+	float angle; //This angle should be in radians, because the math functions we use, use radians.
+} Ball;
 
-Ball triangulate(Ball, Frame, Pad); 
-Ball move_ball(Ball, Frame, Pad);
-float radians(float);
-Pad move_pad(Frame, Pad, char);
-float hit_brick(int, int, Brick, int, Ball);
-void draw_board(Frame, Pad, Ball, Brick);
-Ball hit_something(Ball, int, int, Brick, Frame, int, Pad);
+typedef struct {
+	float x;
+	float y;
+	float length;
+	float height;
+} Pad;
+
+typedef struct {
+	float height;
+	float width;
+	bool gameover;
+} Frame;
+
+typedef struct {
+	float x;
+	float y;
+	float width;
+	float height;
+} Brick;
+
+Ball triangulate(Ball ball, Frame breakout, Pad pad); 
+Ball move_ball(Ball ball, Frame breakout, Pad pad);
+Pad move_pad(Frame breakout, Pad pad, char input);
+Ball hit_brick(int x, int y, Brick bricks[], int size_of_bricks, Ball ball);
+void draw_board(Frame breakout, Pad pad, Ball ball, Brick bricks[], int no_of_bricks);
+Ball hit_something(Ball ball, int x, int y, Brick bricks[], Frame breakout, int size_of_bricks, Pad pad);
 Pad read_input(Pad, Frame, char);
-Ball hit_pad(Ball, Pad);
-Frame gameover();
-Ball hit_wall(Frame, Ball);
+Ball hit_pad(Ball ball, Pad pad);
+Frame gameover(Frame breakout);
+Ball hit_wall(Frame breakout, Ball ball);
+Frame hit_floor(Frame breakout, Ball ball);
 
 #endif
